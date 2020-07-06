23b2e204f117c29159265d2b61de381ce5e32edd
Skmevak <761579099@qq.com>
2020-07-07 01:31:32 +0800

    first

---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="todo-main">
+    <li>
+      <label>
+        <input type="checkbox" />
+        <span>xxxxx</span>
+      </label>
+      <button class="btn btn-danger">删除</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox" />
+        <span>yyyyyy</span>
+      </label>
+      <button class="btn btn-danger">删除</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
